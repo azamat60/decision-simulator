0ac647bbd0e036e132736468360ab090da1881e9
azamataltymyshev <kalyan,number1@gmail.com>
2026-04-22 11:52:40 +0300

    chore: set base path to /decision-simulator in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/decision-simulator',
   test: {
     globals: true,
     environment: 'jsdom',
